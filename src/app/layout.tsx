8ce0c7716e46651a36fe87a1acb3a5d9c1d3e1fd
danuseta <danusetawiardana@gmail.com>
2025-01-14 01:32:53 +0700

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,7 +55,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         {/* Google Analytics */}
         <Script
           strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=G-EYBB6GEMCH"
+          src="https://www.googletagmanager.com/gtag/js?id=G-5PGG1TGNY2"
         />
         <Script
           id="google-analytics"
@@ -65,7 +65,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-EYBB6GEMCH');
+              gtag('config', 'G-5PGG1TGNY2');
             `,
           }}
         />
